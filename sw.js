a38710a3a32304106a25446ca2f8b309cad3a6fb
ThitikornC <tiice01@gmail.com>
2025-11-08 13:28:46 +0700

    calendar Ui

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const CACHE_NAME = 'momay-cache-vB2';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
-  '/style.css?v=2.1',
-  '/script.js?v=2.1',
+  '/style.css?v=2.2',
+  '/script.js?v=2.2',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
