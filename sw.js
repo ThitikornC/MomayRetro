9292932f86a84e8c4431b5b9c674aa07b8b551ba
ThitikornC <tiice01@gmail.com>
2025-11-08 19:07:14 +0700

    ข้อความ commit ของคุณ

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const CACHE_NAME = 'momay-cache-vB2';
 const PRECACHE_URLS = [
   '/',
   '/index.html?',
-  '/style.css?v=2.14',
-  '/script.js?v=2.14',
+  '/style.css?v=2.15',
+  '/script.js?v=2.15',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
